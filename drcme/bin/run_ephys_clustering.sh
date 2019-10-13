f434519b01cd02e560bd02268164f883c97818a2
smestern <smestern@uwo.ca>
2019-10-13 17:51:54 -0400

    Updated config files

--- drcme/bin/run_ephys_clustering.sh
+++ drcme/bin/run_ephys_clustering.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -e
+
 
 CONFIG=$1
 
 # Perform initial clustering
 Rscript run_ephys_clustering.r $CONFIG
-
+read -p "Press enter to continue"
 # Do post-clustering merges
 python run_post_r_merging.py --input_json $CONFIG
 Rscript run_entropy_cumul_merges.r $CONFIG
@@ -15,3 +15,5 @@ Rscript run_cluster_stability.r $CONFIG
 
 # Merge unstable clusters
 python run_merge_unstable_clusters.py --input_json $CONFIG
+
+pause
